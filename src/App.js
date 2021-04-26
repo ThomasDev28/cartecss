0dc07132f535a27336960c9e2efcfc91805a6eae
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-04-26 10:47:20 +0200

    cartefini

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="carte-container">
+        <div className="carte-premiere">
+          <img src="media/LilJay.jpg" alt="liljay"></img>
+          <div className="titre">
+            <h1>Lil Jay</h1>
+            <p>Everything New</p>
+            <p>2014</p>
+
+          </div>
+  
+        </div>
+      <span></span>
+        <div className="carte-deuxieme">
+          <p>Rate this album</p>
+          <div className="icons">
+          <i class="far fa-star"></i>
+          <i class="far fa-star"></i>
+          <i class="far fa-star"></i>
+          <i class="far fa-star"></i>
+          <i class="far fa-star"></i>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
